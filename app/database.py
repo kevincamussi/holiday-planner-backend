845c085e2cdb0eb9bce524de16cf0c516aba3d80
kevincamussi <kevincamussi@gmail.com>
2025-10-03 07:51:01 +0100

    chore: move MongoDB credentials to .env and configure dotend in backend

--- app/database.py
+++ app/database.py
@@ -3,11 +3,18 @@ Database connection setup for MongoDB (Motor async client).
 """
 
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 
-# Connection string (use environment variable in production)
-MONGO_URL = "mongodb+srv://kevin:FLxjLq9uEHt2ZRmO@cluster0.ujf3fpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME")
+
+if not MONGO_URL:
+    raise ValueError("MONGO_URL is not set in environment variables")
 
 # Async client and DB/collection references
 client: AsyncIOMotorClient = AsyncIOMotorClient(MONGO_URL)
-db = client["holiday_planner"]
+db = client["DB_NAME"]
 holidays_collection = db["holidays"]
