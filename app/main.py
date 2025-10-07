6c2627f267188e309abb5bdb143cf045d15541dd
kevincamussi <kevincamussi@gmail.com>
2025-10-07 11:29:00 +0100

    fix(main.py): fix origins

--- app/main.py
+++ app/main.py
@@ -13,15 +13,18 @@ app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(holidays.router, prefix="/holidays", tags=["Holidays"])
 
 # CORS for frontend 
+
+origins=[
+    "https://holiday-planner-frontend-five.vercel.app/",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost",
+    "http://127.0.0.1",
+    ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://holiday-planner-frontend-five.vercel.app/"
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost",
-        "http://127.0.0.1"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
